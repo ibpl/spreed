23da8a2b3dd8a303bc7ddb1a856917cadbf6dcd6
Grigorii K. Shartsev <me@shgk.me>
2024-05-03 19:20:17 +0200

    fix(LeftSidebar): get rid of server styles for settings button
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/LeftSidebar/LeftSidebar.vue
+++ src/components/LeftSidebar/LeftSidebar.vue
@@ -280,12 +280,13 @@
 		</template>
 
 		<template #footer>
-			<div id="app-settings">
-				<div id="app-settings-header">
-					<NcButton class="settings-button" @click="showSettings">
-						{{ t('spreed', 'Talk settings') }}
-					</NcButton>
-				</div>
+			<div class="left-sidebar__settings-button-container">
+				<NcButton type="tertiary" wide @click="showSettings">
+					<template #icon>
+						<Cog :size="20" />
+					</template>
+					{{ t('spreed', 'Talk settings') }}
+				</NcButton>
 			</div>
 		</template>
 	</NcAppNavigation>
@@ -298,6 +299,7 @@ import { ref } from 'vue'
 import AccountMultiplePlus from 'vue-material-design-icons/AccountMultiplePlus.vue'
 import AtIcon from 'vue-material-design-icons/At.vue'
 import ChatPlus from 'vue-material-design-icons/ChatPlus.vue'
+import Cog from 'vue-material-design-icons/Cog.vue'
 import FilterIcon from 'vue-material-design-icons/Filter.vue'
 import FilterRemoveIcon from 'vue-material-design-icons/FilterRemove.vue'
 import List from 'vue-material-design-icons/FormatListBulleted.vue'
@@ -391,6 +393,7 @@ export default {
 		Phone,
 		Plus,
 		ChatPlus,
+		Cog,
 		List,
 		Note,
 		NcEmptyContent,
@@ -1084,13 +1087,13 @@ export default {
 	}
 }
 
+.left-sidebar__settings-button-container {
+	padding: calc(2 * var(--default-grid-baseline));
+}
+
 :deep(.empty-content) {
 	text-align: center;
 	padding: 20% 10px 0;
-}
-
-.settings-button {
-	justify-content: flex-start !important;
 }
 
 :deep(.app-navigation__list) {
