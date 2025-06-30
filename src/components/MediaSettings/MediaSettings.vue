a4dd7f383f77906c2b34db1965026424b14de541
Dorra Jaouad <dorra.jaoued7@gmail.com>
2025-07-01 00:29:25 +0200

    fix: rename show data prop
    Signed-off-by: Dorra Jaouad <dorra.jaoued7@gmail.com>

--- src/components/MediaSettings/MediaSettings.vue
+++ src/components/MediaSettings/MediaSettings.vue
@@ -5,10 +5,10 @@
 
 <template>
 	<component :is="isDialog ? 'NcModal' : 'div'"
-		v-if="modal"
+		v-if="show"
 		:size="isDialog ? 'large' : undefined"
 		:label-id="isDialog ? dialogHeaderId : undefined"
-		@close="closeModal">
+		@close="close">
 		<div class="media-settings">
 			<h2 v-if="isDialog"
 				:id="dialogHeaderId"
@@ -357,7 +357,7 @@ export default {
 
 	data() {
 		return {
-			modal: false,
+			show: false,
 			tabContent: 'devices',
 			audioOn: undefined,
 			videoOn: undefined,
@@ -497,7 +497,7 @@ export default {
 	},
 
 	watch: {
-		modal(newValue) {
+		show(newValue) {
 			if (newValue) {
 				if (this.settingsStore.startWithoutMedia) {
 					// Disable audio
@@ -565,7 +565,7 @@ export default {
 			intermediate: true,
 			handler(value) {
 				if (value) {
-					this.showModal()
+					this.showMediaSettings()
 				}
 			},
 		},
@@ -578,7 +578,7 @@ export default {
 	},
 
 	beforeMount() {
-		subscribe('talk:media-settings:show', this.showModal)
+		subscribe('talk:media-settings:show', this.showMediaSettings)
 		subscribe('talk:media-settings:hide', this.closeModalAndApplySettings)
 
 		// FIXME: this is a workaround to remove the old key from the browser storage
@@ -590,23 +590,23 @@ export default {
 
 	mounted() {
 		if (!this.isDialog) {
-			this.showModal()
+			this.showMediaSettings()
 		}
 	},
 
 	beforeUnmount() {
-		unsubscribe('talk:media-settings:show', this.showModal)
+		unsubscribe('talk:media-settings:show', this.showMediaSettings)
 		unsubscribe('talk:media-settings:hide', this.closeModalAndApplySettings)
 
 		if (!this.isDialog) {
-			this.closeModal()
+			this.close()
 		}
 	},
 
 	methods: {
 		t,
-		showModal(page) {
-			this.modal = true
+		showMediaSettings(page) {
+			this.show = true
 			if (page === 'video-verification') {
 				this.isPublicShareAuthSidebar = true
 			}
@@ -620,8 +620,8 @@ export default {
 			}
 		},
 
-		closeModal() {
-			this.modal = false
+		close() {
+			this.show = false
 			this.updatedBackground = undefined
 			this.audioDeviceStateChanged = false
 			this.videoDeviceStateChanged = false
@@ -677,7 +677,7 @@ export default {
 				emit('local-video-control-button:toggle-video')
 			}
 
-			this.closeModal()
+			this.close()
 		},
 
 		handleUpdateBackground(background) {
